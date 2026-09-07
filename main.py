3b80d01f2aa04cde56ff2bfc689d0cc8c831d03b
wriddhi-codes <314338639+wriddhi-codes@users.noreply.github.com>
2026-09-07 21:13:33 +0530

    updated the code for highlighting the text when it starts with '!'

--- main.py
+++ main.py
@@ -20,16 +20,54 @@ dc = machine.Pin(17, machine.Pin.OUT)
 rst = machine.Pin(16, machine.Pin.OUT)
 busy = machine.Pin(4, machine.Pin.IN)
 
-print("e ink pins config")
+# print("e ink pins config")
+
+# while True:
+#     with open("data.txt", "r") as file:
+#         text_to_display = file.read().strip()
+#         print("Read from file" , text_to_display)
+
+#         y_pos = 50 
+#         for line in text_to_display.split("\n"):
+#             canvas.text(line , 50 , y_pos , 0)
+#             y_pos += 30
+
+
+width = 648
+height = 480
 
 while True:
-    with open("data.txt", "r") as file:
+
+    with open("data.txt","r") as file:
         text_to_display = file.read().strip()
-        print("Read from file" , text_to_display)
+        canvas.fill(1)
 
-        y_pos = 50 
-        for line in text_to_display.split("\n"):
-            canvas.text(line , 50 , y_pos , 0)
-            y_pos += 30
+        canvas.rect(20,20,width-40,height-40,0)
+        canvas.rect(21,21,width-42,height-42,0)
 
-        
+        heading = "DAILY DASHBOARD"
+
+        heading_x = (width - (len(heading)*8))//2
+        canvas.text(heading,heading_x,40,0)
+
+        canvas.hline(40,70,width-80,0)
+
+        y_pos = 100
+
+        for line in text_to_display.split('\n'):
+            # canvas.text(line.strip(), 50 , y_pos, 0)
+            # y_pos += 20
+
+            if line.startswith('!'):
+                actual_text = line[1:].strip()
+                text_width = len(actual_text)
+
+                canvas.fill_rect(45,y_pos-2,text_width+10 , 12 ,0)
+                canvas.text(actual_text,50,y_pos,1)
+            else:
+                canvas.text(line,50,y_pos,0)
+
+            y_pos += 20
+
+
+    time.sleep(5)
--- simul.py
+++ simul.py
@@ -2,17 +2,46 @@ from PIL import Image , ImageDraw , ImageFont
 import os
 
 width , height = 648 , 480
-image = Image.new('1', (width, height), 255)
+image = Image.new('RGB', (width, height), (255,255,255))
 draw  = ImageDraw.Draw(image)
 
-font = ImageFont.truetype("arial.ttf", 20)
+ttile_font = ImageFont.truetype("arial.ttf", 36)
+body_font = ImageFont.truetype("arial.ttf",24)
 
+draw.rectangle([20,20,width-20,height-20],outline=0, width=3)
+
+heading = "DAILY DASHBOARD"
+
+box = draw.textbbox((0,0),heading,font=ttile_font)
+text_width = box[2] -box[0]
+heading_x = (width - text_width) // 2
+
+draw.text((heading_x , 40), heading, font=ttile_font)
+
+draw.line([40,95,width-40,95],fill=0,width=2)
 
 with open("data.txt","r") as file:
     text_to_display = file.read().strip()
 
-draw.text((50,15), text_to_display , fill= 0 , font = font )
+y_pos = 125
+for line in text_to_display.split('\n'):
+    line = line.strip()
+    if line.startswith('!'):
+        actual_text = line[1:].strip()
 
+        tb = draw.textbbox((0,0), actual_text , font=body_font)
+        # draw.text((50,y_pos), line , fill= 0 , font = body_font )
+
+        tw = tb[2]-tb[0]
+        th = tb[3]-tb[1]
+
+        draw.rectangle([45,y_pos-2, 55+tw+10,y_pos+th+6],fill='RED')
+        draw.text((50,y_pos), actual_text, fill="WHITE", font=body_font)
+
+    else:
+        draw.text((50 ,y_pos),line, fill= "BLACK", font=body_font)
+
+    y_pos += 35
 output_path = "preview.png"
 image.save(output_path)
 os.startfile(output_path)
