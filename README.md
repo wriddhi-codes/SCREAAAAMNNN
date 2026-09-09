65d983ce26dae8e1e18f80c6cfa6461dbfced4fb
wriddhi-codes <314338639+wriddhi-codes@users.noreply.github.com>
2026-09-09 17:01:50 +0530

    added readme , made changes to input.cpp to support multiple lines

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # SCREAAAAMNNN
 "SCREAAAAMNNN" is word play for Screen and scream merged together. Screen that is a desktop display for reminders with deadlines and list of task to be done .
+
+
+## Architecture 
+
+1. C++ Desktop editor(imput.cpp) - A lightweight GUI built using raygui and raylib to input the tasks from the user to "data.txt"
+2. Simulation(simul.py) - Reads "data.txt" and renders that a 648*480 visual preview.
+3. ESP32 Firmware(main.py) - Runs on micropython , pulls the tasks from "data.txt" and displays on the E ink screen . 
+
+## File Structure
+
+* input.cpp - Desktop task editor
+* simul.py - PIL based display simulation
+* main.py - ESP32 firmware
+* data.txt - data source for the task list (! demotes important task)
+
+## How to use
+
+1. Compile and run the cpp task editor ( requires Raylib,Raygui and mingw)
+
+>g++ .\input.cpp -o input.exe -I"C:\raylib\raylib\src" -L"C:\raylib\raylib\src" -lraylib -lopengl32 -lgdi32 -lwinmm
+
+2. To run the simulation 
+
+>python simul.py
+
+3. Deploying to ESP32
+
+Flash main.py and upload data.txt to the ESP32 using micropython.
+
+## Images 
+
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+## Resources 
+
+* Raylib
+* Raygui
+* Pillow
+* Framebuf
+* Micropython
--- input.cpp
+++ input.cpp
@@ -14,11 +14,11 @@ int main(){
     int width = GetScreenWidth();
     int height = GetScreenHeight();
 
-    char imp_task[512]="";
-    char task[512]="";
+    char imp_task[3][512]={"","",""};
+    char task[3][512]={"","",""};
 
-    bool edit = false ;
-    bool edit_imp = false ;
+    int edit = -1 ;
+    int edit_imp = -1 ;
 
     float timer = 0.0f;
 
@@ -35,53 +35,69 @@ int main(){
 
         DrawText("TASK EDITOR DASHBOARD",width/2 - MeasureText("TASK EDITOR DASHBOARD",20)/2,30,20,BLACK);
 
-        DrawText("Important Task(Red)",40,65,14,RED);
-        if (GuiTextBox((Rectangle){40,90,570,140},imp_task,512,edit_imp))
+        DrawText("Important Task(Red)",40,90,14,RED);
+        for (int i = 0; i < 3; i++)
         {
-            edit_imp = !edit_imp;
-            edit = false;
-        }
+            if (GuiTextBox((Rectangle){40,float(110 + i*35 ),570,30},imp_task[i],128,edit_imp == i))
+            {
+                if (edit_imp == i)
+                {
+                    edit_imp = -1;
+                }
+                else
+                {
+                    edit_imp = i;
+                }
 
-        DrawText("Important Task(Red)",40,245,14,DARKGRAY);
-        if (GuiTextBox((Rectangle){40,270,570,140},task,512,edit))
-        {
-            edit = !edit ;
-            edit_imp = false;
+                edit = -1;
+            }
         }
+        
+
+        DrawText("Normal Task",40,235,14,DARKGRAY);
+        for (int i = 0; i < 3; i++)
+        {
+            if (GuiTextBox((Rectangle){40,float(250+ i*35),570,30},task[i],512,edit == i))
+            {
+                if (edit == i)
+                {
+                    edit = -1;
+                }
+                else
+                {
+                    edit = i ;
+                }
+
+                edit_imp = -1;
+            }
+        }
+        
 
         if (GuiButton((Rectangle){480,420,130,40},"Save to File"))    
         {
             ofstream outfile("data.txt");
             if (outfile.is_open())
             {
-                stringstream impstream(imp_task);
-                string line;
-                while (getline(impstream,line))
+                for (int i = 0; i < 3; i++)
                 {
+                    string line(imp_task[i]);
+
                     if (!line.empty())
                     {
-                        if (line[0] != '!')
-                        {
-                            outfile << "!" << line << "\n";
-                        }
-                        else{
-                            outfile << line << "\n";
-                        }
-                        
+                        outfile << "!" << line << "\n";
                     }
-                    
+
                 }
 
-                stringstream normstream(task);
-                while (getline(normstream,line))
+                for (int i = 0; i < 3; i++)
                 {
+                    string line(task[i]);
+
                     if (!line.empty())
                     {
-                        outfile << line << "n" ;
+                        outfile << line << "\n" ;
                     }
-                    
-                }
-
+                }   
                 outfile.close();  
                 timer = 2.0f;
 
